1234665fe4a9f25dd5652ea0d72b5f20d820010b
utilisateur <utilisateur@10.13.8.167>
2014-09-11 13:54:53 +0200

    creation modele table + getter setter

--- GestBDE/src/Modele/Dette.java
+++ GestBDE/src/Modele/Dette.java
@@ -16,4 +16,39 @@ public class Dette {
     private int id;
     private ArrayList<Vente> listeVenteImpayee;
     private Eleve eleveEndete;
+    private int montant;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getMontant() {
+        return montant;
+    }
+
+    public void setMontant(int montant) {
+        this.montant = montant;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ArrayList<Vente> getListeVenteImpayee() {
+        return listeVenteImpayee;
+    }
+
+    public void setListeVenteImpayee(ArrayList<Vente> listeVenteImpayee) {
+        this.listeVenteImpayee = listeVenteImpayee;
+    }
+
+    public Eleve getEleveEndete() {
+        return eleveEndete;
+    }
+
+    public void setEleveEndete(Eleve eleveEndete) {
+        this.eleveEndete = eleveEndete;
+    }
+    
+    
 }
