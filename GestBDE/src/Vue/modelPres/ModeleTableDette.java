bb9c36aafa3e4f7164f29d84334faf0531b7604c
utilisateur <utilisateur@10.13.8.167>
2014-09-11 13:41:44 +0200

    Modif IHMs

---
+++ GestBDE/src/Vue/modelPres/ModeleTableDette.java
@@ -0,0 +1,37 @@
+package Vue.modelPres;
+
+
+import Modele.Dette;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author utilisateur
+ */
+public class ModeleTableDette extends AbstractTableModel
+{
+    List<Dette> listeDesDettes;
+    
+    @Override
+    public int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getColumnCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
